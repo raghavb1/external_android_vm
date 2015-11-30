f2920af63bd3e07567e0723c0ddb7e9abb0d83b4
raghav <raghavb1@gmail.com>
2015-11-30 18:23:11 +0530

    keyboard replacement code

--- src/org/mitre/svmp/events/AppsInstallService.java
+++ src/org/mitre/svmp/events/AppsInstallService.java
@@ -72,12 +72,15 @@ public class AppsInstallService {
 
 		File file = new File(localPath); 
 		Boolean success = false;
+		int processResult = 0;
 		if(file.exists()){
 			try {   
 				String command = "pm install " + localPath;
 				Process proc = Runtime.getRuntime().exec(command);
-				proc.waitFor();
-				success = true;
+				processResult = proc.waitFor();
+				if(processResult == 0){
+					success = true;
+				}
 			} catch (Exception e) {
 
 			}

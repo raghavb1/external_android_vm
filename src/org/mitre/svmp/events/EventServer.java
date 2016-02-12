e8baff3e7d83bcd6408396b09ed5fd861c3a3f15
Deept Raghav <raghavb1@gmail.com>
2016-02-12 19:28:08 +0530

    Update EventServer.java

--- src/org/mitre/svmp/events/EventServer.java
+++ src/org/mitre/svmp/events/EventServer.java
@@ -291,9 +291,9 @@ public class EventServer extends BaseServer {
 
     private void injectTouch(MotionEvent me) {
         try {
-            //Log.d(TAG, "injecting touch event");;INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT
+            //Log.d(TAG, "injecting touch event");;INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT;;INJECT_INPUT_EVENT_MODE_ASYNC
             //InputManager.getInstance().injectInputEvent(me,InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH);
-            if (!InputManager.getInstance().injectInputEvent(me,InputManager.INJECT_INPUT_EVENT_MODE_ASYNC))
+            if (!InputManager.getInstance().injectInputEvent(me,InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH))
                 Log.e(TAG, "Failed injecting MotionEvent " + me.toString());
         } catch (Exception e) {
             Log.e(TAG, "Error injecting MotionEvent: " + e.getMessage());

9de27c90c5e8a84fae9217173d483980994b8fbc
Deept Raghav <raghavb1@gmail.com>
2016-04-21 04:32:41 +0530

    Update WebrtcHandler.java

--- src/org/mitre/svmp/events/WebrtcHandler.java
+++ src/org/mitre/svmp/events/WebrtcHandler.java
@@ -157,7 +157,7 @@ public class WebrtcHandler {
                 SessionDescription sdp = new SessionDescription(
                         SessionDescription.Type.fromCanonicalForm(type),
                         //(String) json.get("sdp"));
-                        RemoveAudio((String) json.get("sdp")));
+                        (String) json.get("sdp"));
                 pc.setRemoteDescription(sdpObserver, sdp);
             } else if (type.equals("bye")) {
                 Log.d(TAG, "Remote end hung up; dropping PeerConnection");
@@ -424,6 +424,13 @@ public class WebrtcHandler {
         return newSdpDescription.toString();
     }
 
+    private static String setBitrate(String sdpDescription) {
+    	sdpDescription = sdpDescription.replace( "a=mid:audio\r\n" , "a=mid:audio\r\nb=AS:50\r\n");
+    	sdpDescription = sdpDescription.replace( "a=mid:video\r\n" , "a=mid:video\r\nb=AS:2000\r\na=framerate:24\r\n");
+    	sdpDescription = sdpDescription.replace( "a=mid:data\r\n" , "a=mid:data\r\nb=AS:1638400\r\n");
+    	return sdpDescription;
+    }
+    
     // Mangle SDP to prefer ISAC/16000 over any other audio codec.
     private String preferISAC(String sdpDescription) {
         String[] lines = sdpDescription.split("\n");
@@ -486,7 +493,7 @@ public class WebrtcHandler {
             Log.d(TAG, "Sending " + origSdp.type);
             SessionDescription sdp = new SessionDescription(
                     //origSdp.type, preferISAC(origSdp.description));
-                    origSdp.type, RemoveAudio(origSdp.description));
+                    origSdp.type, setBitrate(origSdp.description));
             JSONObject json = new JSONObject();
             jsonPut(json, "type", sdp.type.canonicalForm());
             jsonPut(json, "sdp", sdp.description);

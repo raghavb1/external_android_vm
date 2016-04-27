00730b714f1edcf742dd4688d788ef3f120fc7a8
oval grey <ovalgrey@ovals-Mac-mini.local>
2016-04-27 15:01:26 +0530

    lots of logging and compression

--- src/org/mitre/svmp/events/StreamHandler.java
+++ src/org/mitre/svmp/events/StreamHandler.java
@@ -1,22 +1,20 @@
 package org.mitre.svmp.events;
 
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.zip.Deflater;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.mitre.svmp.protocol.SVMPProtocol;
 import org.mitre.svmp.protocol.SVMPProtocol.Request;
 import org.mitre.svmp.protocol.SVMPProtocol.Response;
 import org.mitre.svmp.protocol.SVMPProtocol.Response.ResponseType;
 
 import com.google.protobuf.ByteString;
-
-import android.graphics.PixelFormat;
 
 public class StreamHandler{
 
@@ -33,7 +31,6 @@ public class StreamHandler{
 	static FileInputStream graphics = null;
 	static int screenWidth = 360;
 	static int screenHeight = 640;
-	static byte[] piex;
 
 	static FileChannel fc;
 	private static int bufferSize=screenHeight * screenWidth * 2;
@@ -42,14 +39,31 @@ public class StreamHandler{
 	public void handleShareScreenRequest(Request message) throws IOException{
 		//		int [] frameInts = getFrame();
 		while(sendFrames){
-			getScreenBitmap();
-			Response response = buildScreenResponse(ByteString.copyFrom(piex));
-			System.out.println("********************");
+			System.out.println(" ******************** time before bitmap create ********************");
 			System.out.println(System.currentTimeMillis());
+
+			byte[] piex = getScreenBitmap();
+
+			System.out.println(" ******************** time before create response and after bitmap create ********************");
+			System.out.println(System.currentTimeMillis());
+
+			byte [] compressed = compress(piex);
+
+			System.out.println(" ******************** time after compress ********************");
+			System.out.println(System.currentTimeMillis());
+
+			Response response = buildScreenResponse(ByteString.copyFrom(compressed));
+
+			System.out.println("  ********************time after create response ********************");
+			System.out.println(System.currentTimeMillis());
+
 			base.sendMessage(response);
+
+			System.out.println("time after send response ********************");
+			System.out.println(System.currentTimeMillis());
 		}
 	}
-	
+
 	public void setSendFrames(boolean value){
 		this.sendFrames = value;
 	}
@@ -77,55 +91,39 @@ public class StreamHandler{
 
 
 	@SuppressWarnings("resource")
-	public synchronized static void getScreenBitmap() throws IOException {
+	public synchronized static byte[] getScreenBitmap() throws IOException {
 
-		System.out.println("********************");
-		System.out.println(System.currentTimeMillis());
-		
+
 		RandomAccessFile raf = new RandomAccessFile(fbFile, "r");
 		fc = raf.getChannel();
 
 
 		MappedByteBuffer mem = fc.map(FileChannel.MapMode.READ_ONLY, 0, bufferSize);
-		piex = new byte[bufferSize];
+		byte[] piex = new byte[bufferSize];
 		mem.get(piex);
 		fc.close();
 		raf.close();
 
-		System.out.println("********************");
-		System.out.println(piex.length);
+		return piex;
 
-		System.out.println("********************");
-		System.out.println(System.currentTimeMillis());
-		//		
-		//		
-		//		
-		//		
-		//		
-		//		System.out.println("********************");
-		//		System.out.println(System.currentTimeMillis());
-		//		
-		//		PixelFormat pixelFormat = new PixelFormat();
-		//		PixelFormat.getPixelFormatInfo(PixelFormat.RGB_565, pixelFormat);
-		//		int deepth = pixelFormat.bytesPerPixel;
-		//		piex = new byte[screenHeight * screenWidth * deepth];
-		//		
-		//		System.out.println("********************");
-		//		System.out.println(System.currentTimeMillis());
-		//		try {
-		//			graphics = new FileInputStream(fbFile);
-		//		} catch (FileNotFoundException e) {
-		//			e.printStackTrace();
-		//		}
-		//		System.out.println("********************");
-		//		System.out.println(System.currentTimeMillis());
-		//		DataInputStream dStream = new DataInputStream(graphics);
-		//		dStream.readFully(piex);
-		//		dStream.close();
-		//		
-		//		System.out.println("********************");
-		//		System.out.println(System.currentTimeMillis());
 	}
+
+	public static byte[] compress(byte[] data) throws IOException {  
+		Deflater deflater = new Deflater();  
+		deflater.setInput(data);  
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);   
+		deflater.finish();  
+		byte[] buffer = new byte[1024];   
+		while (!deflater.finished()) {  
+			int count = deflater.deflate(buffer); // returns the generated code... index  
+			outputStream.write(buffer, 0, count);   
+		}  
+		outputStream.close();  
+		byte[] output = outputStream.toByteArray();  
+		System.out.println("Original: " + data.length / 1024 + " Kb");  
+		System.out.println("Compressed: " + output.length / 1024 + " Kb");  
+		return output;  
+	} 
 
 }
 
